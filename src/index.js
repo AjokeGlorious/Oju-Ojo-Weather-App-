9791c9501281fba6bbffd971bda571301cbf00c4
AjokeGlorious <131398430+AjokeGlorious@users.noreply.github.com>
2023-11-10 11:58:57 +0100

    updated the  weather description in real time usig the API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 function getWeather(response) {
   let temperatureElement = document.querySelector("#temp")
   let temperature = response.data.temperature.current
-
 let cityElement = document.querySelector("#city");
+let humidityElement = document.querySelector("#humidity");
+let descriptionElement = document.querySelector("#description");
+console.log(response.data.condition.description);
+
+
 cityElement.innerHTML = response.data.city;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 temperatureElement.innerHTML = Math.round(temperature) 
+descriptionElement.innerHTML = response.data.condition.description
 }
 
 function searchCity(city) {
